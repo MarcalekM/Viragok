04b0e583c33c815d42b236f80e71d313700fa9f5
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2024-05-17 08:46:03 +0200

    Add project files.

---
+++ Viragok/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Viragok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Virag> viragok = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\flowers.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) viragok.Add(new(sr.ReadLine()));
+            var v = viragok.OrderBy(v => v._nev).ToList();
+            Feltolt(v);
+        }
+
+        private void Feltolt(List<Virag> viragok)
+        {
+            Viragok.Items.Clear();
+            foreach (var v in viragok)
+            {
+                ListBoxItem virag = new();
+                virag.Content = v._nev;
+                Viragok.Items.Add(virag);
+            }
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            if (Viragok.Items.Count != 0)
+            {
+                ListBoxItem copiedItem = (ListBoxItem)Viragok.SelectedItem;
+                string x = copiedItem.Content.ToString();
+                Masolatok.Items.Add(x);
+            }
+        }
+    }
+}
---
+++ Viragok/Virag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viragok
+{
+    internal class Virag
+    {
+        public string _nev;
+        public int _ar;
+        public string _szin;
+        public string _kep;
+
+        public Virag(string nev, int ar, string szin, string kep)
+        {
+            _nev = nev;
+            _ar = ar;
+            _szin = szin;
+            _kep = kep;
+        }
+        public Virag(string sor)
+        {
+            var adatok = sor.Split(';').ToList();
+            _nev = adatok[0];
+            _ar = int.Parse(adatok[1]);
+            _szin = adatok[2];
+            _kep = adatok[3];
+        }
+    }
+}
